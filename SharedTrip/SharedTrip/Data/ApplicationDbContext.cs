12f06a517ebb0fcc3a4b4615454529e291302c95
Georgi Tsonkov <gtsonkov@abv.bg>
2020-10-20 21:35:43 +0200

    -Create Models

--- SharedTrip/SharedTrip/Data/ApplicationDbContext.cs
+++ SharedTrip/SharedTrip/Data/ApplicationDbContext.cs
@@ -1,9 +1,16 @@
-﻿namespace SharedTrip
-{
-    using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
+using SharedTrip.Models;
 
+namespace SharedTrip
+{
     public class ApplicationDbContext : DbContext
-    { 
+    {
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Trip> Trips { get; set; }
+
+        public DbSet<UserTrip> UserTrip { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
@@ -13,6 +20,19 @@
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<UserTrip>()
+                .HasKey(k => new { k.UserId, k.TripId });
+
+            modelBuilder.Entity<UserTrip>()
+                .HasOne(u => u.User)
+                .WithMany(t => t.UserTrips)
+                .HasForeignKey(u => u.UserId);
+
+            modelBuilder.Entity<UserTrip>()
+                .HasOne(t => t.Trip)
+                .WithMany(u => u.UserTrips)
+                .HasForeignKey(t => t.TripId);
+
             base.OnModelCreating(modelBuilder);
         }
     }
--- SharedTrip/SharedTrip/Data/DatabaseConfiguration.cs
+++ SharedTrip/SharedTrip/Data/DatabaseConfiguration.cs
@@ -3,6 +3,6 @@
     public class DatabaseConfiguration
     {
         public const string ConnectionString =
-            @"Server=.;Database=SharedTrip;Trusted_Connection=True;Integrated Security=True;";
+            @"Server=DESKTOP-O8547PN\TSSQL;Database=SharedTrip;Trusted_Connection=True;Integrated Security=True;";
     }
 }
