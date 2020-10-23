1445a3ccf58ba775e9fa00fa43a43f2403b59dc5
Georgi Tsonkov <gtsonkov@abv.bg>
2020-10-23 17:34:07 +0200

    -add Migrations

---
+++ BattleCards/BattleCards/Migrations/20201023153101_initMigration.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BattleCards.Migrations
+{
+    public partial class initMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cards",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    Name = table.Column<string>(maxLength: 15, nullable: false),
+                    ImageUrl = table.Column<string>(nullable: false),
+                    Keyword = table.Column<string>(nullable: false),
+                    Attack = table.Column<int>(nullable: false),
+                    Health = table.Column<string>(nullable: false),
+                    Description = table.Column<string>(maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cards", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    Username = table.Column<string>(maxLength: 20, nullable: false),
+                    Email = table.Column<string>(nullable: false),
+                    Password = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserCard",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(nullable: false),
+                    CardId = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserCard", x => new { x.UserId, x.CardId });
+                    table.ForeignKey(
+                        name: "FK_UserCard_Cards_CardId",
+                        column: x => x.CardId,
+                        principalTable: "Cards",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserCard_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserCard_CardId",
+                table: "UserCard",
+                column: "CardId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserCard");
+
+            migrationBuilder.DropTable(
+                name: "Cards");
+
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
--- BattleCards/BattleCards/Startup.cs
+++ BattleCards/BattleCards/Startup.cs
@@ -1,15 +1,16 @@
-﻿namespace BattleCards
+﻿using System.Collections.Generic;
+using BattleCards.Data;
+using Microsoft.EntityFrameworkCore;
+using SIS.HTTP;
+using SIS.MvcFramework;
+
+namespace BattleCards
 {
-    using System.Collections.Generic;
-
-    using SIS.HTTP;
-    using SIS.MvcFramework;
-
     public class Startup : IMvcApplication
     {
         public void Configure(IList<Route> routeTable)
         {
-           
+            new ApplicationDbContext().Database.Migrate();
         }
 
         public void ConfigureServices(IServiceCollection serviceCollection)
